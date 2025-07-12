1e24c99f02dfda585657aa98d0d631a2bf030744
Mayank Harplani <harplanimayank5@gmail.com>
2025-07-13 02:06:50 +0530

    auth added, problem model added

---
+++ backend/src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
+import authRoutes from "./routes/auth.routes.js";
+
+
+dotenv.config();
+
+const app = express();
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+app.get('/',(req,res) => {
+     res.send("Welcome to LeetBox");
+})
+
+
+
+app.use("/api/v1/auth",authRoutes);
+
+app.listen(process.env.PORT,() => {
+     console.log('server running');  
+})
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken"
+import {db} from "../libs/db.js"
+export const authMiddleware = async (req,res,next) => {
+    try {
+        const token = req.cookies.jwt;
+
+        if(!token){
+            return res.status(401).json({
+                message: "Unauthorized - no token provided"
+            })
+        }
+
+        let decoded;
+
+        try {
+            decoded = jwt.verify(token,process.env.JWT_SECRET);
+        } catch (error) {
+            return res.status(401).json({
+                message: "Unauthorized - Invalid Token"
+            })
+        }
+        const user = await db.user.findUnique({
+            where: {
+                id: decoded.id
+            },
+            select:{
+                id: true,
+                image: true,
+                name: true,
+                email: true,
+                role: true
+            }
+        });
+
+        if(!user){
+            return res.status(401).json({
+                message: "User Not Found"
+            })
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error("Error authenticating User:", error);
+        res.status(500).json({message: "Error authenticating user"});
+    }
+}
