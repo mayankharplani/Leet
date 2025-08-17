a112de0fe580613ed19b582f46f552815892836e
Mayank Harplani <harplanimayank5@gmail.com>
2025-08-17 23:37:43 +0530

    updated run code controller

--- backend/src/controllers/executeCode.controller.js
+++ backend/src/controllers/executeCode.controller.js
@@ -1,5 +1,9 @@
-import {db} from "../libs/db.js"
-import { getLanguageName, pollBatchResults, submitBatch } from "../libs/judge0.libs.js";
+import { db } from "../libs/db.js";
+import {
+  getLanguageName,
+  pollBatchResults,
+  submitBatch,
+} from "../libs/judge0.libs.js";
 
 export const submitCode = async (req, res) => {
   try {
@@ -39,7 +43,7 @@ export const submitCode = async (req, res) => {
     let allPassed = true;
     // yhn pr hum hr ek test case ko prepare krre hai
     const detailedResults = results.map((result, i) => {
-      // check krlo phle stdout and expected output ko nikl kr ki equal hai ki nhi 
+      // check krlo phle stdout and expected output ko nikl kr ki equal hai ki nhi
       const stdout = result.stdout?.trim();
       const expected_output = expected_outputs[i]?.trim();
       const passed = stdout === expected_output;
@@ -54,85 +58,93 @@ export const submitCode = async (req, res) => {
         compile_output: result.compile_output || null,
         status: result.status.description,
         memory: result.memory ? `${result.memory} KB` : undefined,
-        time: result.time ? `${result.time} s` : undefined
+        time: result.time ? `${result.time} s` : undefined,
       };
     });
     console.log(detailedResults);
     // return;
     const submission = await db.submission.create({
-        data:{
-            userId,
-            problemId,
-            sourceCode: source_code,
-            language: getLanguageName(language_id),
-            stdin: stdin.join("\n"),
-            stdout: JSON.stringify(detailedResults.map((r) => r.stdout)),
-            strerr: detailedResults.some((r) => r.stderr) ? JSON.stringify(detailedResults.map((r) => r.stderr)) : null,
-            compileOutput: detailedResults.some((r) => r.compile_output) ? JSON.stringify(detailedResults.map((r) => r.compile_output)) : null,
-            status: allPassed ? "Accepted" : "Wrong Answer",
-            memory: detailedResults.some((r) => r.memory) ? JSON.stringify(detailedResults.map((r) => r.memory)) : null,
-            time: detailedResults.some((r) => r.time) ? JSON.stringify(detailedResults.map((r) => r.time)) : null
-        }   
-    })
-     
-    // if all test cases are passed all passed true 
-    if(allPassed){
-        await db.problemSolved.upsert({
-            where: {
-                userId_problemId: {userId,problemId}
-            },
-            update: {},
-            create: {
-                userId,problemId
-            }
-        })
+      data: {
+        userId,
+        problemId,
+        sourceCode: source_code,
+        language: getLanguageName(language_id),
+        stdin: stdin.join("\n"),
+        stdout: JSON.stringify(detailedResults.map((r) => r.stdout)),
+        strerr: detailedResults.some((r) => r.stderr)
+          ? JSON.stringify(detailedResults.map((r) => r.stderr))
+          : null,
+        compileOutput: detailedResults.some((r) => r.compile_output)
+          ? JSON.stringify(detailedResults.map((r) => r.compile_output))
+          : null,
+        status: allPassed ? "Accepted" : "Wrong Answer",
+        memory: detailedResults.some((r) => r.memory)
+          ? JSON.stringify(detailedResults.map((r) => r.memory))
+          : null,
+        time: detailedResults.some((r) => r.time)
+          ? JSON.stringify(detailedResults.map((r) => r.time))
+          : null,
+      },
+    });
+
+    // if all test cases are passed all passed true
+    if (allPassed) {
+      await db.problemSolved.upsert({
+        where: {
+          userId_problemId: { userId, problemId },
+        },
+        update: {},
+        create: {
+          userId,
+          problemId,
+        },
+      });
     }
     // save individual test cases results
     const testCaseResults = detailedResults.map((result) => ({
-       submissionId: submission.id,
-       testCase: result.testCase,
-       passed: result.passed,
-       stdout: result.stdout,
-       expected: result.expected,
-       stderr: result.stderr,
-       compileOutput: result.compile_output,
-       status: result.status,
-       memory: result.memory,
-       time: result.time
-    }))
+      submissionId: submission.id,
+      testCase: result.testCase,
+      passed: result.passed,
+      stdout: result.stdout,
+      expected: result.expected,
+      stderr: result.stderr,
+      compileOutput: result.compile_output,
+      status: result.status,
+      memory: result.memory,
+      time: result.time,
+    }));
 
     await db.testCaseResult.createMany({
-      data: testCaseResults
-    })
+      data: testCaseResults,
+    });
 
     const submissionWithTestCase = await db.submission.findUnique({
       where: {
-        id: submission.id
+        id: submission.id,
       },
       include: {
-        testCases: true
-      }
-    })
+        testCases: true,
+      },
+    });
     res.status(200).json({
       success: true,
       message: "Code Executed!",
-      submission : submissionWithTestCase
+      submission: submissionWithTestCase,
     });
   } catch (error) {
-    console.log("Error executing Code: ",error);
+    console.log("Error executing Code: ", error);
     res.status(500).json({
-      error: "Failed to execute code"
+      error: "Failed to execute code",
     });
   }
 };
 
-
-
-export const runCode = async (req,res) => {
-  const {source_code,language_id,stdin,expected_outputs,problemId} = req.body;
+export const runCode = async (req, res) => {
+  const { source_code, language_id, stdin, expected_outputs, problemId } =
+    req.body;
   const userId = req.user.id;
   try {
-   if (
+    if (
       !Array.isArray(stdin) ||
       stdin.length === 0 ||
       !Array.isArray(expected_outputs) ||
@@ -142,24 +154,46 @@ export const runCode = async (req,res) => {
         error: "Invalid or Missing Test Cases",
       });
     }
-     const submissions = stdin.map((input) => ({
-        source_code,
-        language_id,
-        stdin: input,
-     }));
+    const submissions = stdin.map((input) => ({
+      source_code,
+      language_id,
+      stdin: input,
+    }));
 
-     const submitResponse = await submitBatch(submissions);
-     const tokens = submitResponse.map((res) => res.token);
-     const results = await pollBatchResults(tokens);
-     res.status(200).json({
+    const submitResponse = await submitBatch(submissions);
+    const tokens = submitResponse.map((res) => res.token);
+    const results = await pollBatchResults(tokens);
+    let allPassed = true;
+
+    const testCaseResults = results.map((result, i) => {
+      const stdout = result.stdout?.trim();
+      const expected_output = expected_outputs[i]?.trim();
+      const passed = stdout === expected_output;
+      if (!passed) allPassed = false;
+
+      return {
+        testCase: i + 1,
+        passed,
+        stdout,
+        expected: expected_output,
+        stderr: result.stderr || null,
+        compile_output: result.compile_output || null,
+        status: result.status.description,
+        memory: result.memory ? `${result.memory} KB` : undefined,
+        time: result.time ? `${result.time} s` : undefined,
+      };
+    });
+
+
+    res.status(200).json({
       success: true,
       message: "Code Successfully Running",
-      results
-     })
+      testCaseResults,
+    });
   } catch (error) {
-    console.log("Error executing Code: ",error);
+    console.log("Error executing Code: ", error);
     res.status(500).json({
-      error: "Failed to execute code"
+      error: "Failed to execute code",
     });
   }
-}
+};
